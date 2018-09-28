be5bfb147dadb3ce8b7e1d1f7d46f1318ec92852
Daniel <danny@Mihaelas-MacBook-Pro.local>
2018-09-28 16:48:33 -0400

    Starting from scratch

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,8 +4,3 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Welcome to the home site')
