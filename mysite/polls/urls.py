8c480d809342a8c49a4d63a43ab20859d3b0bc81
Daniel <lupas.danny@gmail.com>
2018-10-05 19:11:23 -0400

    Added more complicated views and urls for the polls app
    I created a template for the polls homepage that shows a list of all the questions. Additionally, if you type a question id, eg. polls/1, it will the question text. Also if you cleck on the question text from the homepage it will send you to the details page. Finally, I added in 404 error if the question you are looking for does not exist.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/votes/$', views.vote, name='vote'),
+
+
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Question
 
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Welcome to the home site')
+    questions = Question.objects.all().order_by('pub_date')
+    return render(request, 'polls/home.html', {'questions': questions})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
